dfbd848c3cdfed58832443d619f66bc047b18d85
kevin <segaud.kevin@gmail.com>
2019-05-08 18:11:43 +0200

    fake authentication service

---
+++ services/authentication_service/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:authentication_service/authentication_service.dart' as authentication_service;
+
+main(List<String> arguments) {
+  print('Hello world: ${authentication_service.calculate()}!');
+}
