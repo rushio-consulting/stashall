017949ed01c11aa30362286394f248ef274fccfd
kevin <segaud.kevin@gmail.com>
2019-05-31 12:56:36 +0200

    fix signin

--- app/lib/src/routes/signin_route.dart
+++ app/lib/src/routes/signin_route.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:rc_router/rc_router.dart';
+import 'package:stashall/src/views/signin/signin.dart';
 
 class SigninRoute extends RcRoute {
   static String routePath = '/signin';
